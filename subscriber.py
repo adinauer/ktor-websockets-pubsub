0294ecc82f6959228d1dfedbca79ccd023638441
Alexander Dinauer <alexander@dinauer.at>
2019-02-20 20:16:33 +0100

    initial commit

---
+++ subscriber.py
@@ -0,0 +1,12 @@
+import pulsar
+
+client = pulsar.Client('pulsar://localhost:6650')
+consumer = client.subscribe('my-topic',
+                            subscription_name='my-sub')
+
+while True:
+    msg = consumer.receive()
+    print("Received message: '%s'" % msg.data())
+    consumer.acknowledge(msg)
+
+client.close()
